20b98569756e360ce00bdbcf51fd9b33046c2051
Rafał Rączka <info@rafalraczka.com>
2022-06-26 22:45:17 +0200

    emacs: define default profile
    Signed-off-by: Rafał Rączka <info@rafalraczka.com>

---
+++ .config/emacs/profiles.el
@@ -0,0 +1,31 @@
+;;; profiles.el --- List of profiles for Emacs -*- lexical-binding: t -*-
+
+;; Copyright (C) 2022 Rafał Rączka
+
+;; Author: Rafał Rączka <info@rafalraczka.com>
+;; URL: https://github.com/rafalraczka/config-emacs
+
+;; This file is NOT part of GNU Emacs.
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 3, or (at your option)
+;; any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;;; Code:
+
+(
+ ("rafalraczka-main" . ((user-emacs-directory . "rafalraczka-main")))
+ )
+
+;;; profiles.el ends here
